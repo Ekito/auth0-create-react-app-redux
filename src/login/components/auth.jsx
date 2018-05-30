fa16b3849d8e772285e34bccf733be4594944dbe
Olivier Bearn <olivier.bearn@innov-atm.com>
2018-05-30 10:32:56 +0200

    Support url params in private routes

--- src/login/components/auth.jsx
+++ src/login/components/auth.jsx
@@ -1,13 +1,15 @@
 import { Route, Redirect } from 'react-router-dom';
 import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
 class PrivateRouteComponent extends Route {
 
   render() {
     const Component = this.props.component;
-    return this.props.login.loggedIn ? <Component {...this.props} /> : <Redirect
+    const mergedPropsAfterMatch = Object.assign({}, this.props, {match: this.state.match});
+    console.log(mergedPropsAfterMatch);
+    return this.props.login.loggedIn ? <Component {...mergedPropsAfterMatch} /> : <Redirect
       to={{
         pathname: '/login',
         state: { from: this.props.location.pathname },
@@ -23,25 +25,3 @@ const mapStateToProps = state => ({
 export const PrivateRoute = withRouter(connect(
   mapStateToProps,
 )(PrivateRouteComponent));
-
-export default PrivateRoute;
-
-/*export const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => (rest.loggedIn ?
-            (<Component {...props} />) : (
-              <Redirect
-                to={{
-              pathname: '/login',
-              state: { from: props.location.pathname },
-            }}
-              />
-        ))
-      }
-  />);
-
-PrivateRoute.propTypes = {
-  component: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  location: PropTypes.shape({ pathname: PropTypes.string }).isRequired,
-};*/
